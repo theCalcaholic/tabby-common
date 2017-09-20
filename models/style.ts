7282275442631c257e6bf8f6d4114bd74ce1bf25
Tobias Knöppler (3knoeppl) <3knoeppl@informatik.uni-hamburg.de>
2017-09-20 12:13:31 +0200

    Move models to separate directory. Add Style model.

---
+++ models/style.ts
@@ -0,0 +1,4 @@
+export class Style {
+  protected static template:string;
+  protected parameters:Array<any>;
+}
---
+++ styles/classicbrown.style.ts
@@ -0,0 +1,80 @@
+import { Style } from '../models/style';
+
+export class ClassicBrownStyle extends Style {
+  protected parameters = [
+    {backgroundUrl: "https://www.bienenfisch-design.com/wp-content/uploads/wpsg_produktbilder/6410/tn/s-800-600-feine-pergament-textur-003.jpg"},
+  ];
+  protected static template = `body {
+background-image: url(\${backgroundUrl});
+background-repeat: no-repeat;
+background-size: cover;
+min-height: 400px;
+}
+.tab {
+visibility: hidden;
+appearance: none;
+border-color: red;
+border-width: 1px;
+border-style: solid;
+min-height: 400px;
+}
+.tab + div {
+display: none;
+}
+.tab:checked + div {
+display: block;
+}
+.tabcontent p, .tabtitle, .tabcontent li {
+font-family: Calibri, Arial, sans-serif;
+color: #880F0F;
+font-size: medium;
+}
+.tabcontent a {
+color: #FF2222;
+}
+.tabcontent ul {
+list-style: none;
+padding-left: 0;
+}
+.tabcontent li {
+margin-bottom: 1em;
+}
+.tabcontent img {
+max-width: 300px;
+margin: 10px;
+border-style: solid;
+border-width: 2px;
+border-color: #440808;
+}
+.tabtitle {
+margin: 5px 10px;
+padding: 5px;
+font-size: 1.1em;
+line-height: 2em;
+background-color: #880F0F;
+color: white;/*#440808;*/
+cursor: pointer;
+/*font-weight: bold;*/
+border-radius: 5px;
+}
+.tabtitle:hover {
+background-color: #EEEEEE;
+color: #880F0F;
+}
+/*.profilepic {
+border-width: 2px;
+border-style: solid;
+margin: 20px 30px;
+width: 250px;
+height: 273px;
+border-radius: 15em;
+float: right;
+}*/
+.contentcontainer {
+padding: 10px;
+min-height: 80%;
+}
+#editmode .tab + div {
+display: block;
+}`;
+}
