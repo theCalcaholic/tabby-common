f1ef079f5af383405d2a985357ec875de87e846c
theCalcaholic <3knoeppl@informatik.uni-hamburg.de>
2017-09-27 22:48:08 +0200

    Update TestStyle CSS.

--- styles/test.style.ts
+++ styles/test.style.ts
@@ -1,11 +1,81 @@
 import { Style } from '../models/style';
 
-export class TestStyle extends Style {
-  id = "testStyle";
-  protected template = `\
+let cssTemplate = `\
 body {
   background-image: url(http://www.planwallpaper.com/static/images/303836.jpg);
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+.tab {
+  visibility: hidden;
+  appearance: none;
+  border-color: red;
+  border-width: 1px;
+  border-style: solid;
+}
+.tab + div {
+  display: none;
+}
+.tab:checked + div {
+  display: block;
+}
+.tabcontent p, .tabtitle, .tabcontent li {
+  font-family: Calibri, Arial, sans-serif;
+  color: #880F0F;
+  font-size: medium;
+}
+.tabcontent a {
+  color: #FF2222;
+}
+.tabcontent ul {
+  list-style: none;
+  padding-left: 0;
+}
+.tabcontent li {
+  margin-bottom: 1em;
+}
+.tabcontent img {
+  max-width: 300px;
+  margin: 10px;
+  border-style: solid;
+  border-width: 2px;
+  border-color: #440808;
+}
+.tabtitle {
+  margin: 5px 10px;
+  padding: 5px;
+  font-size: 1.1em;
+  line-height: 2em;
+  background-color: #880F0F;
+  color: white;/*#440808;*/
+  cursor: pointer;
+  /*font-weight: bold;*/
+  border-radius: 5px;
+}
+.tabtitle:hover {
+  background-color: #EEEEEE;
+  color: #880F0F;
+}
+/*.profilepic {
+  border-width: 2px;
+  border-style: solid;
+  margin: 20px 30px;
+  width: 250px;
+  height: 273px;
+  border-radius: 15em;
+  float: right;
+}*/
+.contentcontainer {
+  padding: 10px;
+  min-height: 80%;
+}
+#editmode .tab + div {
+  display: block;
 }`;
+
+export class TestStyle extends Style {
+  id = "testStyle";
+  protected template = cssTemplate;
   parameters = [
     {
       id: "testParam",
