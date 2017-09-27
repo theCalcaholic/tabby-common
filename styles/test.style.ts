ab5f29bc9d16e838c1154e32db3572d596d2060b
theCalcaholic <3knoeppl@informatik.uni-hamburg.de>
2017-09-27 21:56:59 +0200

    Add background image to TestStyle.

--- styles/test.style.ts
+++ styles/test.style.ts
@@ -2,7 +2,7 @@ import { Style } from '../models/style';
 
 export class TestStyle extends Style {
   id = "testStyle";
-  protected template = "body {background-color: blue;}";
+  protected template = "body {background: url(http://www.planwallpaper.com/static/images/303836.jpg);}";
   parameters = [
     {
       id: "testParam",
