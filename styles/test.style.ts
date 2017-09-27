353bad060b33882d013d8f73ed39b8ebab9085fd
theCalcaholic <3knoeppl@informatik.uni-hamburg.de>
2017-09-27 22:43:59 +0200

    Fix TestStyle background image.

--- styles/test.style.ts
+++ styles/test.style.ts
@@ -2,7 +2,10 @@ import { Style } from '../models/style';
 
 export class TestStyle extends Style {
   id = "testStyle";
-  protected template = "body {background: url(http://www.planwallpaper.com/static/images/303836.jpg);}";
+  protected template = `\
+body {
+  background-image: url(http://www.planwallpaper.com/static/images/303836.jpg);
+}`;
   parameters = [
     {
       id: "testParam",
