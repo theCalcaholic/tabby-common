37b1ed2a24e6bb9a57dd23695796301e9ae83227
theCalcaholic <3knoeppl@informatik.uni-hamburg.de>
2017-09-27 08:02:07 +0200

    Update profile model with style information.

--- models/profile.ts
+++ models/profile.ts
@@ -1,21 +1,28 @@
 import { Tab, TabData, tabFromData } from './tab';
+import { Parameter } from './style';
 
 export class ProfileData {
   id: string;
   tabs: TabData[]
   title: string;
+  styleId: string;
+  styleParameters: Parameter[];
 }
 
 export class Profile {
   id: string;
   title: string;
   tabs: Tab[];
+  styleId: string;
+  styleParameters: Parameter[];
 
   toData(): ProfileData {
     let data = new ProfileData();
     data.id = this.id;
     data.title = this.title || '';
     data.tabs = this.tabs.map((tab) => tab.toData());
+    data.styleId = this.styleId;
+    data.styleParameters = this.styleParameters;
     return data;
   }
 }
@@ -28,5 +35,7 @@ export function profileFromData(data: ProfileData): Profile {
     data.tabs.forEach((tab) => {
       profile.tabs.push(tabFromData(tab));
     });
+    profile.styleId = data.styleId;
+    profile.styleParameters = data.styleParameters;
     return profile;
 }
