7e2bcb9c0244f22a38de69bc1bb63e5617d2d32a
theCalcaholic <3knoeppl@informatik.uni-hamburg.de>
2017-09-27 05:38:22 +0200

    Add test style to exported styles list.

--- styles/styles.ts
+++ styles/styles.ts
@@ -1,11 +1,13 @@
 import { ClassicBrownStyle } from './classicbrown.style';
+import { TestStyle } from './test.style';
 
 class DefaultStyle extends ClassicBrownStyle {
   id: 'default';
 }
 
 let styles = [
-  ClassicBrownStyle
+  ClassicBrownStyle,
+  TestStyle
 ];
 
 
